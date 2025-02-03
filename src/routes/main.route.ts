06ce5ff43820f2be8898da86bbad09ea4a239791
OnlyForward0613 <daveescott059@gmail.com>
2025-02-03 04:50:11 -0500

    feature: complete import and export wallet

--- src/routes/main.route.ts
+++ src/routes/main.route.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-import { insertMovie, Movies, Update, Delete } from "../controller/movie.route";
+// import { insertMovie, Movies, Update, Delete } from "../controller/movie.route";
 import { createAndBuy, distribution } from "../controller/pumpfun.route";
-import { generateCommonWallets, generateDevWallet, importWallets } from "../controller/wallet.route";
+import { exportWallets, generateCommonWallets, generateDevWallet, generateSniperWallet, importFundWallet, importWallets } from "../controller/wallet.route";
 import schema from "./schema";
 import validator, { ValidationSource } from "../helper/validator";
 
@@ -19,7 +19,10 @@ router.post("/distribution", distribution);
 
 router.get("/generate-wallet/common", validator(schema.generateCommonWallets, ValidationSource.QUERY), generateCommonWallets);
 router.get("/generate-wallet/dev", generateDevWallet);
+router.get("/generate-wallet/sniper", generateSniperWallet);
 
-router.post("/import-wallets", validator(schema.importWallets), importWallets);
+router.post("/import-wallet", validator(schema.importWallets), importWallets);
+router.post("/import-wallet/fund", validator(schema.importFundWallet), importFundWallet);
+router.get("/export-wallet", exportWallets);
 
 export default router;
